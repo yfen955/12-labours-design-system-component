459cc60c0882007c758bbaff3559567d49b15a5d
Charlie Cui <charliezhi123@gmail.com>
2021-08-24 18:45:23 +1200

    Create a 'checkbox' component and change its style to the standard one

---
+++ src/stories/checkbox/checkbox.stories.js
@@ -0,0 +1,82 @@
+import Checkbox from '@/components/Checkbox/src/Checkbox.vue'
+import './demo-styles.scss';
+
+export default {
+    title: 'Components/Checkbox',
+    includeStories: []
+  }
+  
+  const createDemo = (data, val) => {
+    return {
+      components: { Checkbox },
+      data() {
+        return {
+          data: data,
+          checkboxVal: val
+        }
+      },
+      template: `
+        <div class="checkbox-group">
+          <checkbox
+            v-for="item in data"
+            v-bind:key="item.label"
+            v-model="checkboxVal"
+            :label="item.label"
+            :disabled="item.disabled || false"
+            :display="item.display"
+          />
+        </div>
+      `
+    }
+  }
+  
+  export const Primary = () => createDemo(
+    [
+      {
+        label: 1,
+        display: "Option 1"
+      },
+      {
+        label: 2,
+        display: "Option 2"
+      },
+      {
+        label: 3,
+        display: "Option 3"
+      }
+    ],
+    1
+  )
+  
+  
+  export const Disabled = () => createDemo(
+    [
+      {
+        label: 1,
+        display: "Checked & Disabled",
+        disabled: true
+      },
+      {
+        label: 2,
+        display: "Unchecked & Disabled",
+        disabled: true
+      }
+    ],
+    1
+  )
+  
+  export const Single = () => createDemo(
+    [
+      {
+        label: 1,
+        display: "Option 1"
+      },
+      {
+        label: 2,
+        display: "Option 2"
+      }
+    ],
+    1
+  )
+  
+  
