0ed2856dfba1144383db59c1a3eac927b79ffc25
Riffat Nourin <rnou112@aucklanduni.ac.nz>
2021-12-16 13:35:17 +1300

    Updated event from 'change' to 'input' in Multiline

--- src/components/MultilineText/src/MultilineText.vue
+++ src/components/MultilineText/src/MultilineText.vue
@@ -6,10 +6,10 @@
       :placeholder="placeholderText"
       :resize="resize"
       :maxlength="maxLength"
-      :minLength="minLength"
+      :minlength="minLength"
       :rows="numRows"
       :disabled="isDisabled"
-      @change="textChange"
+      @input="textChange"
     />
     <div class="label-word-count">
       Max length: {{maxLength}} - Used: {{value.length}} - Remaining: {{maxLength-(value.length)}}
@@ -31,8 +31,7 @@ export default {
       required:true
     },
     minLength: {
-      type: Number,
-      default:3
+      type: Number
     },
     resize: {
       type: String,
@@ -62,7 +61,8 @@ export default {
   
   methods: {
     textChange: function(input) {
-      this.showMinSpan=this.value.length<this.minLength
+      if(this.minLength)
+        this.showMinSpan=this.value.length<this.minLength
       this.$emit('text-change', input)
     }
   }
