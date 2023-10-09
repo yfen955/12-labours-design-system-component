b17b5f92eb5a8c6d1708c48fe1d3393b3b00a7ab
Frances Feng <arthur.f.kirkland.jones@gmail.com>
2023-10-10 12:35:04 +1300

    fix display img error

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,23 @@
+const path = require('path')
+
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   configureWebpack: {
+    resolve: {
+      alias: {
+        '@': resolve('src')
+      }
+    }
+  },
+  chainWebpack(config) {
+    config.module.rule("images")
+      .test(/\.(jpe?g|png|gif|svg)$/)
+      .use('url-loader')
+      .loader('url-loader').options({
+        name: 'img/[name]_[hash:6].[ext]'
+      })
   }
 }
